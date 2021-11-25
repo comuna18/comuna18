7a68b93cac7bde07ec1610efaee31a7b23f503f9
Joseph Sierra <57599376+Deathpandk@users.noreply.github.com>
2021-11-25 16:34:24 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='comuna18',
-    version='0.0.5',
+    version='0.0.6',
     author='Joseph Sierra',
     author_email='joseph.sierra@comuna18.com',
     description='Comuna 18 base build',
